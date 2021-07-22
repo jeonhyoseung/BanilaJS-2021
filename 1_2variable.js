af80253cce2fe5fcedef7788c9e0ecad09791eb4
jeonhyoseung <wlsdnrrh@naver.com>
2021-07-22 23:56:45 +0900

    recap

--- 1_2variable.js
+++ 1_2variable.js
@@ -1,15 +1,15 @@
 
 console.log(5454545454);
-console.log(5+2);
-console.log(5*2);
-console.log(5/2);
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
 
 const a = 5;
 const b = 2;
 //  변수로 하면 반복 작업 지울 수 있음
-console.log(a*b);
-console.log(a/b);
-console.log(a+b);
+console.log(a * b);
+console.log(a / b);
+console.log(a + b);
 
 const veryLongVariableName = "nico";
-console.log("Hello" + veryLongVariableName);
+console.log("Hello" + veryLongVariableName);//
